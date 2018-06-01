8d0e93f35438325e0af16c84b81826d4d1c0f3f3
jßan <ahsan.wtc@gmail.com>
2018-06-01 17:50:57 +0200

    adding express server

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -7,9 +7,11 @@ const optimizationConfig = require('./webpack.opt.config');
 const productionConfig = function (env) {
     const NODE_ENV = env.NODE_ENV ? env.NODE_ENV : 'development';
     return {
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
-        })
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+            }),
+        ]
     };
 }
 
